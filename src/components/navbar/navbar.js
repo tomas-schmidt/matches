67d62543d51a5d329b76bf2fef242ee1e009d6f3
Tomas <tomas.schmidt@wolox.com.ar>
2018-11-01 21:11:39 -0300

    add navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <span>Título</span>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
