37a3799d38d88b9ac48169306bf902c1642ea5b3
Tomas <tomas.schmidt@wolox.com.ar>
2018-11-02 23:08:35 -0300

    add navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="Navbar">
-        <span>Título</span>
+      <div className="nav">
+        <span className="title">Futbol Argentino</span>
+        <div className="section-list">
+          <a href="#" className="nav-item"><span className="section">Copa Libertadores</span></a>
+          <a href="#" className="nav-item"><span className="section">Copa Sudamericana</span></a>
+          <a href="#" className="nav-item"><span className="section">Superliga</span></a>
+          <a href="#" className="nav-item"><span className="section">Copa Argentina</span></a>
+        </div>
       </div>
     );
   }
