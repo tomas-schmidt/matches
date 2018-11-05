f24f8f752502240484f74abf76548468cbd50cf7
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-05 13:07:16 -0300

    change layout

--- src/components/match/match.js
+++ src/components/match/match.js
@@ -6,12 +6,14 @@ class Match extends Component {
     let competition;
     if (this.props.competition) competition = <span>{  this.props.competition }</span>
     return (
-      <div className="match-container">
-        <span>{ this.props.match.time }</span>
-        <span>{ this.props.match.homeTeam }</span>
-        <img className="team-logo" src={  this.props.match.homeTeamLogo }/>
-        <span>{ this.props.match.visitingTeam }</span>
-        <img className="team-logo" src={ this.props.match.visitingTeamLogo }/>
+      <div className="container">
+        <div className="match-container">
+          <span>{ this.props.match.time }</span>
+          <span>{ this.props.match.homeTeam }</span>
+          <img className="team-logo" src={  this.props.match.homeTeamLogo }/>
+          <img className="team-logo" src={ this.props.match.visitingTeamLogo }/>
+          <span>{ this.props.match.visitingTeam }</span>        
+        </div>
         { competition }
       </div>
     );
