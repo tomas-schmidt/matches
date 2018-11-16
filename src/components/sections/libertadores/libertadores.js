086a113461d17774f9e5ca727ce1551a4111809b
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-16 11:20:55 -0300

    add integration with api

--- src/components/sections/libertadores/libertadores.js
+++ src/components/sections/libertadores/libertadores.js
@@ -7,43 +7,11 @@ import environment from '../../../environments/environment';
 class Libertadores extends Component {
   constructor(props) {
     super(props);
-    this.state = { matches: null }
+    this.state = { matches: [] }
   }
 
   getMatches() {
-    this.state.matches = [ 
-      {
-        "id": 0,
-        "homeTeam": "Team1 Team1",
-        "visitingTeam": "Team2 Team2",
-        "homeTeamLogo": "../favicon.ico",
-        "visitingTeamLogo": "../favicon.ico",
-        "time": "17:30",
-        "day": "Hoy",
-        "instance": "8vos"
-      },
-      {
-        "id": 1,
-        "homeTeam": "Team1 Team1",
-        "visitingTeam": "Team2 Team2",
-        "homeTeamLogo": "../favicon.ico",
-        "visitingTeamLogo": "../favicon.ico",
-        "time": "17:30",
-        "day": "17/05",
-        "instance": "8vos"
-      },
-      {
-        "id": 2,
-        "homeTeam": "Team1 Team1",
-        "visitingTeam": "Team2 Team2",
-        "homeTeamLogo": "../favicon.ico",
-        "visitingTeamLogo": "../favicon.ico",
-        "time": "17:30",
-        "day": "17/05",
-        "instance": "8vos"
-      }
-    ]
-    axios.get(`${environment.apiBaseUrl}/libertadores`).then(res => { console.log(res); })
+    axios.get(`${environment.apiBaseUrl}/libertadores`).then(res => { this.setState({ matches: res.data }); });
   }
 
   render() {
