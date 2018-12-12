e601a77094669a4918dafdd96862d0a9d70d12a7
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-12-12 11:13:27 -0300

    add table component integrations with endpoint

--- src/components/sections/competition/competition.js
+++ src/components/sections/competition/competition.js
@@ -19,7 +19,7 @@ class Competition extends Component {
       <div>
         <span className={`${this.props.comp.type}-title`}>{this.props.comp.name}</span>
         <Matches matches={this.state.matches}></Matches>
-        <Table table={this.state.table}></Table>
+        <Table competition={this.props.comp.name} table={this.state.table}></Table>
       </div>
     );
   }
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -3,8 +3,14 @@ import './table.css';
 
 class Table extends Component {
   render() {
+    console.log(this.props.table);
     return (
-        <span>Table! {this.props.table}</span>
+      <div>
+        <span>{this.props.competition}</span>
+        <div className="table-container">
+          {this.props.table.map(team => <span key={team.id_team}>{team.team_name}</span>)}
+        </div>
+      </div> 
     );
   }
 }
