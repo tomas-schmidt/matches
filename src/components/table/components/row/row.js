1d86496bfe08d7b1003d303ca21f14f5994750ea
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-12-12 13:50:53 -0300

    add row styles

--- src/components/table/components/row/row.js
+++ src/components/table/components/row/row.js
@@ -5,13 +5,13 @@ class Row extends Component {
   render() {
     let rowClass = this.props.data.is_title ? 'row-title' : 'row-basic';
     return (
-    <div className={rowClass}>
-      <span>{this.props.data.team_name}</span>
-      <span>{this.props.data.points}</span>
-      <span>{this.props.data.matches_played}</span>
-      <span>{this.props.data.goals}</span>
-      <span>{this.props.data.goals_recieved}</span>
-    </div>
+      <div className={rowClass}>
+        <span>{this.props.data.team_name}</span>
+        <span>{this.props.data.points}</span>
+        <span>{this.props.data.matches_played}</span>
+        <span>{this.props.data.goals}</span>
+        <span>{this.props.data.goals_recieved}</span>
+      </div>
     );
   }
 }
