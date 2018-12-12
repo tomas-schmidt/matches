659c38cf957e2213f03502cfaeadf3f21e8547fb
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-12-12 16:42:09 -0300

    add styles

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -4,7 +4,7 @@ import Row from './components/row/row'
 
 class Table extends Component {
   render() {
-    let title = { 'is_title': true, 'team_name': 'Equipo', 'points': 'Puntos', 'matches_played': 'PJ', 'goals': 'Goles', 'goals_recieved': 'GEC' };
+    let title = { 'is_title': true, 'team_name': 'Equipo', 'points': 'Pts', 'matches_played': 'PJ', 'goals': 'G', 'goals_recieved': 'GE' };
     return (
       <div className="table-container">
         <Row key={0} data={title}></Row>
