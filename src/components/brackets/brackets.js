bf42b477ab33e2f1cf178406ec31ac45d118183c
Tomas <tomas.schmidt@wolox.com.ar>
2018-12-20 22:23:26 -0300

    add bracket component

--- src/components/brackets/brackets.js
+++ src/components/brackets/brackets.js
@@ -3,6 +3,7 @@ import './brackets.css'
 
 class Brackets extends Component {
   render() {
+    let table = [{"id_competition":1,"id_team":1,"goals":10,"goals_recieved":10,"matches_played":10,"points":10,"id_instance":10,"team_name":"River","instanceName":"8vos","instanceDetail":"2"},{"id_competition":1,"id_team":2,"goals":10,"goals_recieved":10,"matches_played":10,"points":10,"id_instance":10,"team_name":"Boca","instanceName":"8vos","instanceDetail":"2"},{"id_competition":1,"id_team":3,"goals":10,"goals_recieved":10,"matches_played":10,"points":10,"id_instance":10,"team_name":"Racing","instanceName":"8vos","instanceDetail":"2"},{"id_competition":1,"id_team":4,"goals":10,"goals_recieved":10,"matches_played":10,"points":10,"id_instance":10,"team_name":"Independiente","instanceName":"8vos","instanceDetail":"2"}];
     return (
       <div>
           Brackets
---
+++ src/components/brackets/components/bracket/bracket.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './bracket.css'
+
+class Bracket extends Component {
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default Bracket;
