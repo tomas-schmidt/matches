0020ae2788c39872034f89d0cfa0402d716d4ce5
Tomas <tomas.schmidt@wolox.com.ar>
2018-12-22 16:42:07 -0300

    refactor matches component

--- src/components/matches/matches.js
+++ src/components/matches/matches.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import Match from './components/match/match'
 import './matches.css';
+import matchesService from '../../services/matches-service.js'
+import Loader from '../loader/loader';
 
 class Matches extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { matches: [], loading: true };
+  }
+
+  componentWillMount() {
+    matchesService.getMatches(this.props.id).then(res => { this.setState({ matches: res.data, loading: false }) });
+  }
+
   render() {
-    let description;
-    (this.props.matches.length) ? description = 'Partidos' : description = 'No se han encontrado partidos :(' ;
+    let description = 'No se han encontrado partidos :(';
+    if (!this.state.loading && this.state.matches.length) description = 'Partidos';
+    let header = this.state.loading ?
+      <Loader></Loader> :
+      <span className="description">{description}</span>
     return (
       <div className="matches-container">
-        <span className="description">{description}</span>
-        {this.props.matches.map(match => <Match key={match.id} match={match}></Match>)}
+        {header}
+        {this.state.matches.map(match => <Match key={match.id} match={match}></Match>)}
       </div>
     );
   }
--- src/components/sections/competition/competition.js
+++ src/components/sections/competition/competition.js
@@ -2,34 +2,20 @@ import React, { Component } from 'react';
 import './competition.css'
 import Matches from '../../matches/matches';
 import Table from '../../table/table';
-import matchesService from '../../../services/matches-service.js'
-import Loader from '../../loader/loader';
 import Brackets from '../../brackets/brackets';
 
 
 class Competition extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { matches: [], table: [], loading: true };
-  }
-
-  componentWillMount() {
-    matchesService.getCompetition(this.props.id).then(res => { this.setState({ matches: res.data.matches, table: res.data.table, loading: false }); });
-  }
-
   render() {
-    let competitionContent = !this.state.loading ? 
-      <div className="competition-info">
-        <Matches matches={this.state.matches}></Matches>
-        <Table table={this.state.table}></Table>
-        <Brackets></Brackets>
-      </div> 
-      : <Loader></Loader>;
-      let type = this.props.comp.isLocal ? 'local' : 'inter';
+    let type = this.props.comp.isLocal ? 'local' : 'inter';
     return (
       <div className="competition-container">
         <span className={`${type}-title comp-title`}>{this.props.comp.name}</span>
-        {competitionContent}
+        <div className="competition-info">
+          <Matches key={this.props.id} id={this.props.id}></Matches>
+          {/* <Table hasTable={this.props.comp.hasTable} key={this.props.id} id={this.props.id}></Table>; */}
+          {/* <Brackets hasBrackets={this.props.comp.hasBrackets} key={this.props.id} id={this.props.id}></Brackets> */}
+        </div>
       </div>
     );
   }
