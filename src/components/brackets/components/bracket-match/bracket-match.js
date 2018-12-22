717ecd42a731ffa49a736d819200fb8e4aa9bcfc
Tomas <tomas.schmidt@wolox.com.ar>
2018-12-22 19:44:59 -0300

    add bracket match component

---
+++ src/components/brackets/components/bracket-match/bracket-match.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './bracket-match.css'
+
+class BracketMatch extends Component {
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default BracketMatch;
