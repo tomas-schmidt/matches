2de3ed9d9100403aa54f08c843c90165c511024a
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-12-27 11:52:16 -0300

    add groups component

---
+++ src/components/groups/components/group/group.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './group.css';
+
+class Group extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Group;
---
+++ src/components/groups/groups.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './groups.css';
+
+class Groups extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Groups;
