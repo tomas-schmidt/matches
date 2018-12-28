e91f15fbe61b9e508d2bf76372654cac96876593
Tomas <tomas.schmidt@wolox.com.ar>
2018-12-28 11:57:29 -0300

    finish brackets display

--- src/components/brackets/components/bracket-match/bracket-match.js
+++ src/components/brackets/components/bracket-match/bracket-match.js
@@ -4,8 +4,9 @@ import './bracket-match.css'
 class BracketMatch extends Component {
   render() {
     if (!this.props.match) return null;
+    let matchStyles = this.props.isFinal ? 'bracket-final' : '';
     return (
-      <div className="bracket-match">
+      <div className={`bracket-match ${matchStyles}`}>
         <span className="bracket-team">{this.props.match.team1}</span>
         <span className="bracket-team">{this.props.match.team2}</span>
       </div>
