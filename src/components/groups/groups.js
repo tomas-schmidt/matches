132fab401a9a5ee04e8502695ab2731849863bd4
Tomas <tomas.schmidt@wolox.com.ar>
2018-12-30 20:19:14 -0300

    add api integration to get groups

--- src/components/groups/groups.js
+++ src/components/groups/groups.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './groups.css';
 import Loader from '../loader/loader';
+import matchesService from '../../services/matches-service.js'
+import Table from '../table/table'
 
 class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { groups: [], loading: true }
+  }
+
+  componentWillMount() {
+    matchesService.getGroups(this.props.id).then(res => { this.setState({ groups: res.data, loading: false }) });
+  }
+
   render() {
+    console.log(this.state.groups); // TODO: Fix groups rendering
+    if (this.state.loading) return <Loader></Loader>;
     return (
       <div className="groups-container">
-        <span>GROUPS!</span>
-        <Loader></Loader>
+        <span className="description">Grupos</span>
+        {this.state.groups.map(group => <Table key={group} table={group}></Table>)}
       </div>
     );
   }
