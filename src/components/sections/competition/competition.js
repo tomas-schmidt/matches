e94bd44bc28079bc12ca88e6be3bbab5e61166a5
Tomas <tomas.schmidt@wolox.com.ar>
2019-01-03 23:11:15 -0300

    change data fetch logic

--- src/components/sections/competition/competition.js
+++ src/components/sections/competition/competition.js
@@ -4,9 +4,19 @@ import Matches from '../../matches/matches';
 import PositionsTable from '../../positions-table/positions-table';
 import Brackets from '../../brackets/brackets';
 import Groups from '../../groups/groups';
+import matchesService from '../../../services/matches-service.js'
 
 
 class Competition extends Component {
+  componentWillMount() {
+    const fetchData1 = matchesService.getGroups(this.props.id).then(res => { console.log('fetchdata1') });
+    const fetchData2 = matchesService.getMatches(this.props.id).then(res => { console.log('fetchdata2') });
+
+    Promise.all([ fetchData1, fetchData2 ]).then((responses) => {
+      console.log(responses);
+    });
+  }
+
   render() {
     let type = this.props.comp.isLocal ? 'local' : 'inter';
     let table = this.props.comp.hasPositionsTable ? <PositionsTable id={this.props.id}></PositionsTable> : null;
