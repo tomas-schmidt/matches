92dbc7c26554cc4707059b1fd08bafa1c5431f90
Tomas <tomas.schmidt@wolox.com.ar>
2019-01-04 01:59:18 -0300

    groom code

--- src/components/sections/competition/competition.js
+++ src/components/sections/competition/competition.js
@@ -7,41 +7,30 @@ import Groups from '../../groups/groups';
 import matchesService from '../../../services/matches-service.js'
 import Loader from '../../loader/loader';
 
-
 class Competition extends Component {
   constructor(props) {
     super(props);
     this.state = { loading: true }
-    this.table = null;
-    this.brackets = null;
-    this.groups = null;
-    this.matches = null;
+    this.table = this.brackets = this.groups = this.matches = null;
   }
 
   componentWillMount() {
-    const p1 = this.props.comp.hasGroups ? matchesService.getGroups(this.props.id).then(res => { this.groups = <Groups groups={res.data}></Groups> }) : null;
-    const p2 = matchesService.getMatches(this.props.id).then(res => { this.matches = <Matches matches={res.data}></Matches> });
-    const p3 = this.props.comp.hasBrackets ? matchesService.getBrackets(this.props.id).then(res => { this.brackets = <Brackets brackets={res.data}></Brackets>  }) : null;
-    const p4 = this.props.comp.hasPositionsTable ? matchesService.getTable(this.props.id).then(res => { this.table = <PositionsTable table={res.data}></PositionsTable> }): null;
-    Promise.all([p1, p2, p3, p4]).then((res) => { this.setState({ loading: false }) });
+    const p1 = this.props.comp.hasGroups ? matchesService.getGroups(this.props.id).then(res => this.groups = <Groups groups={res.data}></Groups>) : null;
+    const p2 = matchesService.getMatches(this.props.id).then(res => this.matches = <Matches matches={res.data}></Matches>);
+    const p3 = this.props.comp.hasBrackets ? matchesService.getBrackets(this.props.id).then(res => this.brackets = <Brackets brackets={res.data}></Brackets>) : null;
+    const p4 = this.props.comp.hasPositionsTable ? matchesService.getTable(this.props.id).then(res => this.table = <PositionsTable table={res.data}></PositionsTable>): null;
+    Promise.all([p1, p2, p3, p4]).then(res => this.setState({ loading: false }));
   }
 
   render() {
-    let type = this.props.comp.isLocal ? 'local' : 'inter';
     if (this.state.loading) return <Loader></Loader>
-
+    const type = this.props.comp.isLocal ? 'local-title' : 'inter-title';
     return (
       <div className="competition-container">
-        <span className={`${type}-title comp-title`}>{this.props.comp.name}</span>
+        <span className={`${type} comp-title`}>{this.props.comp.name}</span>
         <div className="competition-info">
-          <div className="section-one">
-            {this.matches}
-            {this.table}
-            {this.brackets}
-          </div>
-          <div className="section-two">
-            {this.groups}
-          </div>
+          <div className="section-one">{this.matches} {this.table} {this.brackets}</div>
+          <div className="section-two">{this.groups}</div>
         </div>
       </div>
     );
